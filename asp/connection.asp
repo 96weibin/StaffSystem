1b2e58cfdf7202b5cb87ae7c659ef7cdfa920d63
96weibin <1643960119@qq.com>
2018-10-06 13:49:18 +0800

    gulp压缩管理文件

--- asp/connection.asp
+++ asp/connection.asp
@@ -1,6 +1,6 @@
 <%
-	'链接数据库  这样是不是会暴露我的数据库 的密码 密码  老哥 请对我下手轻点
 	set Conn = server.CreateObject("adodb.connection")
-	Conn.ConnectionString = "Driver=SQL Server;Server=iZ2ze2ezw3wajpZ\WEIBINSERVER;UID=dev;PWD=dev;DataBase=dev"
+	Conn.ConnectionString = "Driver=SQL Server;Server=47.95.8.174\WEIBINDB;UID=dev;PWD=dev;DataBase=Development"
 	Conn.Open
+	RESPONSE.WRITE(CONN.STATE)
 %>
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+let gulp = require('gulp');
+let babel = require('gulp-babel');
+let minifyCss = require('gulp-minify-css');
+let imagemin = require('gulp-imagemin');
+let uglify = require('gulp-uglify');
+
+
+gulp.task('js', ()=>{
+    gulp.src('./dev/js/*.js')
+    .pipe(babel({
+        presets:['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+})
+
+gulp.task('css',()=>{
+    gulp.src('./dev/css/*.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('dist/css'))
+})
+gulp.task('img',()=>{
+    gulp.src('./dev/img/*.*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+})
+
+gulp.task('default',()=>{
+    gulp.watch('./dev/js/*.js',['js'])
+    gulp.watch('./dev/css/*.css',['css'])
+    gulp.watch('./dev/img/*.*',['img'])
+})
